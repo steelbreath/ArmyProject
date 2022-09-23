6c1924d4b9d965daa967a4345b497d1bbf6edc37
steelbreath <zlatnikovyevgeniy@gmail.com>
2022-09-23 19:02:41 +0300

    add connection pool

---
+++ src/main/java/com/solvd/army/connectionPool/BasicConnectionPool.java
@@ -0,0 +1,101 @@
+package com.solvd.army.connectionPool;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BasicConnectionPool implements ConnectionPool{
+    private String url;
+    private String user;
+    private String password;
+    private List<Connection> connectionPool;
+    private List<Connection> usedConnections = new ArrayList<>();
+    private final static int INITIAL_POOL_SIZE = 5;
+    private final static int MAX_POOL_SIZE = 10;
+    private final static int MAX_TIMEOUT = 5;
+
+    public static BasicConnectionPool create(String url, String user, String password) throws SQLException {
+        List<Connection> pool = new ArrayList<>(INITIAL_POOL_SIZE);
+        for (int i = 0; i < INITIAL_POOL_SIZE; i++) {
+            pool.add(createConnection(url, user, password));
+        }
+        return new BasicConnectionPool(url, user, password, pool);
+    }
+
+    public BasicConnectionPool() {}
+    public BasicConnectionPool(String url, String user, String password, List<Connection> pool) {}
+    public BasicConnectionPool(String url, String user, String password, List<Connection> connectionPool,
+                               List<Connection> usedConnections) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+        this.connectionPool = connectionPool;
+        this.usedConnections = usedConnections;
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        if (connectionPool.isEmpty()) {
+            if (usedConnections.size() < MAX_POOL_SIZE) {
+                connectionPool.add(createConnection(url, user, password));
+            } else {
+                throw new RuntimeException(
+                        "Maximum pool size reached, no available connections!");
+            }
+        }
+
+        Connection connection = connectionPool
+                .remove(connectionPool.size() - 1);
+
+        if(!connection.isValid(MAX_TIMEOUT)){
+            connection = createConnection(url, user, password);
+        }
+
+        usedConnections.add(connection);
+        return connection;
+    }
+    @Override
+    public boolean releaseConnection(Connection connection) {
+        connectionPool.add(connection);
+        return usedConnections.remove(connection);
+    }
+    private static Connection createConnection(
+            String url, String user, String password)
+            throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+    public int getSize() {
+        return connectionPool.size() + usedConnections.size();
+    }
+    @Override
+    public String getUrl() {
+        return url;
+    }
+    @Override
+    public String getUser() {
+        return user;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    public List<Connection> getConnectionPool() {
+        return connectionPool;
+    }
+    public List<Connection> getUsedConnections() {
+        return usedConnections;
+    }
+    public static int getInitialPoolSize() {
+        return INITIAL_POOL_SIZE;
+    }
+    public void shutdown() throws SQLException {
+        usedConnections.forEach(this::releaseConnection);
+        for (Connection c : connectionPool) {
+            c.close();
+        }
+        connectionPool.clear();
+    }
+}
---
+++ src/main/java/com/solvd/army/connectionPool/ConnectionPool.java
@@ -0,0 +1,12 @@
+package com.solvd.army.connectionPool;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionPool {
+    Connection getConnection() throws SQLException;
+    boolean releaseConnection(Connection connection);
+    String getUrl();
+    String getUser();
+    String getPassword();
+}
